3d831e086069ba51e47ad575fd703249aa71f014
Asher Foa <1268088+asherf@users.noreply.github.com>
2021-10-02 22:13:21 -0700

    set env

---
+++ set_env.sh
@@ -0,0 +1,5 @@
+#10020  python3 -m posthog.app.gunicorn_main
+#export DATABASE_URL=postgres://posthog:little8jerry483seinfeld@localhost:5434/posthog
+#export REDIS_URL=redis://localhost/
+# export SECRET_KEY=infkjklsdjflk9fdsfu0sdfdfn
+#  docker run -p 6379:6379  -it redis:6-alpine

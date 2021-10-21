545c307efbb8ddaa42d0a60b0cdd438e8f6e6c15
Asher Foa <1268088+asherf@users.noreply.github.com>
2021-10-20 21:03:28 -0700

    move stuff to toolchain specific folder

--- posthog/toolchain/web/gunicorn_main.py
+++ posthog/toolchain/web/gunicorn_main.py
@@ -2,15 +2,16 @@
 import os
 import django
 from gunicorn.app.wsgiapp import WSGIApplication
- 
+
+
 class PosthogWSGIApplication(WSGIApplication):
-    
-    _WSGI_APP = "posthog.app.wsgi:application"
+
+    _WSGI_APP = "posthog.toolchain.app.wsgi:application"
    
     def load_config(self) -> None:
         self.cfg.set("default_proc_name", self._WSGI_APP)
         self.app_uri = self._WSGI_APP
-        conf_path = "python:posthog.app.gunicorn_conf"
+        conf_path = "python:posthog.toolchain.app.gunicorn_conf"
         self.load_config_from_file(conf_path)
         self.cfg.set("config", conf_path)
 

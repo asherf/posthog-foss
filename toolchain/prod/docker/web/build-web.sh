292f850062dcd70462eb07a59716361ac6e7e8ce
Asher Foa <1268088+asherf@users.noreply.github.com>
2021-10-20 21:57:10 -0700

    build frontend

--- toolchain/prod/docker/web/build-web.sh
+++ toolchain/prod/docker/web/build-web.sh
@@ -6,9 +6,17 @@ cd "$(git rev-parse --show-toplevel)"
 
 IMAGE_TAG=posthog/web:latest
 WORKDIR=toolchain/prod/docker/web
-echo "Building Posthog Web image at rev ${IMAGE_TAG} ..."
 
-# remote_tag="${IMAGE_REGISTRY:-}/posthog:web:${IMAGE_TAG:-dev}"
+echo "Build frontend web app"
+yarn --frozen-lockfile
+
+
+
+
+echo "Build gunicorn PEX"
 ./toolchain/prod/docker/builder/dockers.sh package package posthog/toolchain/web:gunicorn-app
 cp dist.docker/posthog.toolchain.web/gunicorn-app.pex "${WORKDIR}/gunicorn.pex"
+
+# remote_tag="${IMAGE_REGISTRY:-}/posthog:web:${IMAGE_TAG:-dev}"
+echo "Building Posthog Web image at rev ${IMAGE_TAG} ..."
 docker build --file="${WORKDIR}/Dockerfile.web" "${WORKDIR}" -t "${IMAGE_TAG}"

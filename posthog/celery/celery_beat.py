e30b8429c8bdc77d67455d5247a4e61e2abe2134
Asher Foa <1268088+asherf@users.noreply.github.com>
2021-10-20 17:33:29 -0700

    add celery entry point

---
+++ posthog/celery/celery_beat.py
@@ -0,0 +1,14 @@
+import sys
+from pathlib import Path
+from celery.bin.celery import main as celery_main
+
+
+def start():
+    pid = Path("celerybeat.pid")
+    if not pid.exists():
+        sys.exit(celery_main(argv=["celery", "-A", "posthog", "beat", "-S", "redbeat.RedBeatScheduler"]))
+    else:
+        pid.unlink()
+
+if __name__ == "__main__":
+    start()

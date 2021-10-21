aa63f3f26cdd8124ad3d83168c52f7a0c8cd232e
Asher Foa <1268088+asherf@users.noreply.github.com>
2021-10-20 21:53:08 -0700

    ignore pex files

--- toolchain/prod/docker/web/build-web.sh
+++ toolchain/prod/docker/web/build-web.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 IMAGE_TAG=posthog/web:latest
-WORKDIR=toolchain/prod/docker
+WORKDIR=toolchain/prod/docker/web
 echo "Building Posthog Web image at rev ${IMAGE_TAG} ..."
 
 # remote_tag="${IMAGE_REGISTRY:-}/posthog:web:${IMAGE_TAG:-dev}"

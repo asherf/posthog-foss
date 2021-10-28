42772a1c73da4905f972bb4b156f55079c83c543
Asher Foa <1268088+asherf@users.noreply.github.com>
2021-10-27 17:53:01 -0700

    build pex first

--- toolchain/prod/docker/web/prep-build-web-ci.sh
+++ toolchain/prod/docker/web/prep-build-web-ci.sh
@@ -4,8 +4,11 @@ set -euo pipefail
 
 cd "$(git rev-parse --show-toplevel)"
 
-IMAGE_TAG=posthog/web:latest
 WORKDIR=toolchain/prod/docker/web
+
+echo "Build gunicorn PEX"
+./pants package package posthog/toolchain/web:gunicorn-app
+cp dist/posthog.toolchain.web/gunicorn-app.pex "${WORKDIR}/gunicorn.pex"
 
 echo "Build frontend web app"
 yarn --frozen-lockfile
@@ -13,7 +16,3 @@ yarn build
 rm -rf "${WORKDIR}/frontend-dist"
 mkdir "${WORKDIR}/frontend-dist"
 cp -r "frontend/dist/" "${WORKDIR}/frontend-dist"
-
-echo "Build gunicorn PEX"
-./pants package package posthog/toolchain/web:gunicorn-app
-cp dist.docker/posthog.toolchain.web/gunicorn-app.pex "${WORKDIR}/gunicorn.pex"

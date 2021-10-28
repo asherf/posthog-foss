f4274bb92692160d89d843f5c8ea41d70dbd958e
Asher Foa <1268088+asherf@users.noreply.github.com>
2021-10-27 19:08:12 -0700

    gunicorn conf update

--- posthog/toolchain/web/gunicorn_conf.py
+++ posthog/toolchain/web/gunicorn_conf.py
@@ -3,25 +3,23 @@ keepalive = 120
 timeout = 90
 grateful_timeout = 120
 
-
+# https://textkool.com/en/ascii-art-generator?hl=default&vl=default&font=Doom&text=PostHog%20%2B%20Toolchain
 def on_starting(server):
     print(
         """
 \x1b[1;34m"""
         + r"""
- _____          _   _    _             
-|  __ \        | | | |  | |            
-| |__) |__  ___| |_| |__| | ___   __ _ 
-|  ___/ _ \/ __| __|  __  |/ _ \ / _` |
-| |  | (_) \__ \ |_| |  | | (_) | (_| |
-|_|   \___/|___/\__|_|  |_|\___/ \__, |
-                                  __/ |
-                                 |___/ 
+______         _   _   _                       _____           _      _           _       
+| ___ \       | | | | | |                _    |_   _|         | |    | |         (_)      
+| |_/ /__  ___| |_| |_| | ___   __ _   _| |_    | | ___   ___ | | ___| |__   __ _ _ _ __  
+|  __/ _ \/ __| __|  _  |/ _ \ / _` | |_   _|   | |/ _ \ / _ \| |/ __| '_ \ / _` | | '_ \ 
+| | | (_) \__ \ |_| | | | (_) | (_| |   |_|     | | (_) | (_) | | (__| | | | (_| | | | | |
+\_|  \___/|___/\__\_| |_/\___/ \__, |           \_/\___/ \___/|_|\___|_| |_|\__,_|_|_| |_|
+                                __/ |                                                     
+                               |___/                                                      
 """
         + """
 \x1b[0m
 """
     )
     print("Server running on \x1b[4mhttp://{}:{}\x1b[0m".format(*server.address[0]))
-    print("Questions? Please shoot us an email at \x1b[4mhey@posthog.com\x1b[0m")
-    print("\nTo stop, press CTRL + C")
